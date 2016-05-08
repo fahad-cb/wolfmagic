f2d4c30fcfb2406358868e6782c93787f3e4059e
Saqib Razzaq <saqi.cb@gmail.com>
2016-05-08 13:09:32 +0500

    got_ffmpeg (python) added

--- python/includes/classes/helper.py
+++ python/includes/classes/helper.py
@@ -1,9 +1,24 @@
 class HuntHelp(object):
+	global ffmpeg
+	ffmpeg = 'C:/xamppPHP7/htdocs/wolfmagic.git/trunk/libs/ffmpeg.exe'
 	"""docstring for WolfMagic"""
 	def getExtension(self, file):
-		import os.path
 		extension = os.path.splitext(file)[1]
 		return extension
+
+	def isFile(self, fname):
+		import os
+		if (os.path.isfile(fname)):
+			return fname
+		else:
+			return False
+
+	def isWindows(self):
+		import os
+		if (os.name == 'nt'):
+			return True
+		else:
+			return False
 
 	def cmd(self, cmd, out = True):
 		import os
@@ -33,3 +48,22 @@ class HuntHelp(object):
 		if (msg):
 			print ext + " is invalid audio file"
 		return False
+
+	def got_ffmpeg(self, path = False, msg = False):
+		if (self.isWindows()):
+			global ffmpeg
+			path = ffmpeg
+		
+		if (not path):
+			ffmpeg = self.cmd('which ffmpeg')
+		else:
+			ffmpeg = self.isFile(path)
+
+		if (ffmpeg):
+			if (msg):
+				print 'FFMPEG is installed at ' + ffmpeg
+			return ffmpeg
+		else:
+			if (msg):
+				print 'FFMPEG installation not found'
+			return False
