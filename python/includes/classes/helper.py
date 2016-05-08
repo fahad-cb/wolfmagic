c58f1c71f97ca945bc747fd308c8def2476a65c2
Saqib Razzaq <saqi.cb@gmail.com>
2016-05-08 12:27:35 +0500

    Added multiple functions for python

--- python/includes/classes/helper.py
+++ python/includes/classes/helper.py
@@ -5,10 +5,31 @@ class HuntHelp(object):
 		extension = os.path.splitext(file)[1]
 		return extension
 
+	def cmd(self, cmd, out = True):
+		import os
+		data = os.system(cmd)
+		return data
+
 	def is_video(self, path, msg = False):
 		formats = ['mp4','MP4', 'wmv', 'webm', 'ogv', 'mov', '3gp','flv', 'MPEG', 'mpeg', 'mpeg4']	
 		ext = self.getExtension(path)
 		for vtype in formats:
 			if (ext == "."+vtype):
+				if (msg):
+					print ext + " is valid video "
 				return ext
+		if (msg):
+			print ext + " is invalid video file"
+		return False
+
+	def is_audio(self, path, msg = False):
+		formats = ['mp3', 'wav', 'aac', 'ogg', 'oga', 'wav', 'wma', 'webm']	
+		ext = self.getExtension(path)
+		for vtype in formats:
+			if (ext == "."+vtype):
+				if (msg):
+					print ext + " is valid audio "
+				return ext
+		if (msg):
+			print ext + " is invalid audio file"
 		return False
