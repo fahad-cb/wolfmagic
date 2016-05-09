a4a60c4d9375eb9eee5654961cc5573ca816dfc0
Saqib Razzaq <saqi.cb@gmail.com>
2016-05-09 14:31:44 +0500

    C++ structure added

---
+++ c++/.gitignore
@@ -0,0 +1,4 @@
+import sys
+sys.path.append( 'C:/xamppPHP7/htdocs/wolfmagic.git/trunk/python/includes/' )
+from common import *
+helper.is_video("main.mp4")
---
+++ c++/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+using std::cout;
+using std::string;
+using std::endl;
+
+int main(int argc, char* argv[]) {
+	//cout << strlen("aSaqib");
+	cout << argv[1] << endl;
+	//cout << argc;
+	//sqrt(2, 5);
+
+	return 0;
+}
